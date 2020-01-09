f03c43dbd9d04cde23c38248559c9725506582c8
nanjingchj <nanjingchj@gmail.com>
2020-01-09 07:25:52 -0500

    structure finished

--- src/me/nanjingchj/jmessenger/plugin/about/Main.kt
+++ src/me/nanjingchj/jmessenger/plugin/about/Main.kt
@@ -2,6 +2,9 @@ package me.nanjingchj.jmessenger.plugin.about
 
 import jmessenger.client.AbstractPlugin
 import jmessenger.shared.Message
+import java.awt.GridBagConstraints
+import java.awt.GridBagLayout
+import javax.swing.JButton
 import javax.swing.JComponent
 import javax.swing.JPanel
 
@@ -12,9 +15,35 @@ class Main : AbstractPlugin() {
     override fun onStart() {}
 
     override fun getCustomJComponent(): JComponent? {
-        val pnl = JPanel()
+        val pnl = JPanel(GridBagLayout())
         pnl.name = "About"
 
+        var c = GridBagConstraints()
+        c.gridx = 0
+        c.gridy = 0
+        c.weightx = 1.0
+        c.weighty = 0.0
+        c.ipady = 100
+        c.fill = GridBagConstraints.BOTH
+        pnl.add(JButton("Image"), c)
+
+        c = GridBagConstraints()
+        c.gridx = 0
+        c.gridy = 1
+        c.weightx = 1.0
+        c.weighty = 0.0
+        c.ipady = 200
+        c.fill = GridBagConstraints.BOTH
+        pnl.add(JButton("Information"), c)
+
+        // the spring
+        c = GridBagConstraints()
+        c.gridx = 0
+        c.gridy = 2
+        c.weightx = 1.0
+        c.weighty = 1.0
+        c.fill = GridBagConstraints.BOTH
+        pnl.add(JButton("I am a spring"), c)
         return pnl
     }
 }
